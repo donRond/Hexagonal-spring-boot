154242c59cf3719dfd2f5f621c522289daccc79d
Ronald J <ronald.juscosta@gmail.com>
2024-07-25 13:26:36 -0300

    test

--- src/main/java/br/com/rest/api/ApiApplication.java
+++ src/main/java/br/com/rest/api/ApiApplication.java
@@ -1,6 +1,5 @@
 package br.com.rest.api;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
